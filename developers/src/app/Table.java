4cdab4ecd461eaeb0cc485a807f0ece3f962190e
Brian Gibson <bggibson@hotmail.com>
2019-03-26 14:59:27 -0400

    Adds functionailty to add Reservation to Table

--- developers/src/app/Table.java
+++ developers/src/app/Table.java
@@ -1,12 +1,24 @@
 package app;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Table {
 
     private final String id;
     private final int capacity;
+    private final List<Reservation> reservations = new ArrayList<>();
 
     public Table(String id, int capacity) {
         this.id = id;
         this.capacity = capacity;
+    }
+
+    public boolean add(Reservation reservation) {
+        int currentTotal = reservations.stream().mapToInt(r -> r.getSize()).sum();
+        int remainingCapacity = capacity - currentTotal;
+        if (reservation.getSize() < remainingCapacity)
+            return reservations.add(reservation);
+        return false;
     }
 }
--- developers/tst/app/TableTest.java
+++ developers/tst/app/TableTest.java
@@ -1,7 +1,12 @@
 package app;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class TableTest {
@@ -10,5 +15,29 @@ public class TableTest {
     public void createsTable() {
         Table table = new Table("A", 10);
         assertTrue(table instanceof Table);
+    }
+
+    @Test
+    public void adding_reservation_to_table_with_capacity_succeeds() {
+        Table table = new Table("A", 10);
+        List<String> list = new ArrayList<String>() {{
+            add("Thornton");
+            add("Taylor");
+        }};
+        Reservation reservation = new Reservation("Owens", 3, list);
+
+        assertTrue(table.add(reservation));
+    }
+
+    @Test
+    public void adding_reservation_to_table_without_capacity_fails() {
+        Table table = new Table("A", 10);
+        List<String> list = new ArrayList<String>() {{
+            add("Thornton");
+            add("Taylor");
+        }};
+        Reservation reservation = new Reservation("Owens", 11, list);
+
+        assertFalse(table.add(reservation));
     }
 }
