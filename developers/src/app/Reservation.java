101677f5a41fadca3f24f58d47c6ec28522de3f7
Brian Gibson <bggibson@hotmail.com>
2019-03-26 14:56:51 -0400

    Adds getSize to Reservation; Renames number to size

--- developers/src/app/Reservation.java
+++ developers/src/app/Reservation.java
@@ -4,12 +4,16 @@ import java.util.List;
 
 public class Reservation {
     private final String id;
-    private final int number;
+    private final int size;
     private final List<String> constraints;
 
-    public Reservation(String id, int number, List<String> constraints) {
+    public Reservation(String id, int size, List<String> constraints) {
         this.id = id;
-        this.number = number;
+        this.size = size;
         this.constraints = constraints;
+    }
+
+    public int getSize() {
+        return size;
     }
 }
--- developers/tst/app/ReservationTest.java
+++ developers/tst/app/ReservationTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class ReservationTest {
@@ -17,5 +18,15 @@ public class ReservationTest {
         }};
         Reservation reservation = new Reservation("Owens", 3, list);
         assertTrue(reservation instanceof Reservation);
+    }
+
+    @Test
+    public void reservationHasCorrectSize(){
+        List<String> list = new ArrayList<String>() {{
+            add("Thornton");
+            add("Taylor");
+        }};
+        Reservation reservation = new Reservation("Owens", 3, list);
+        assertEquals(3,reservation.getSize());
     }
 }
